227738197d955c03e8ba09299ccec0c43511391f
David <david.sienz@outlook.com>
2023-03-08 15:27:28 +0100

    strings-zahlen

---
+++ 02 Strings-Zahlen/script.js
@@ -0,0 +1,9 @@
+let a = 17.4;
+let b = 3.9;
+
+let result = 17.4 / 3.9
+console.log("The result of " + a + " divided by " + b + " = " + result);
+
+let firstName = "Damien";
+let lastName = "Wallis";
+console.log("Ich heiße " + firstName + " " + lastName + ".");
