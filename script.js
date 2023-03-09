45d1383cf7ae02a5327ce399e3dede31498344e1
David <david.sienz@outlook.com>
2023-03-09 13:38:24 +0100

    rename project

---
+++ script.js
@@ -0,0 +1,27 @@
+let banana = "Banane";
+let apple = "Apfel";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let bananAverageWeightInKilo = 0.22;
+let appleAverageWeightInKilo = 0.34;
+
+let bananaPricePerPiece = 2.14*0.22;
+let applePricePerPiece = 3.43*0.34;
+
+
+
+console.log("Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel");
+
+let eightApples = applePricePerPiece*8
+console.log("Acht Äpfel kosten " + eightApples + "€");
+
+let seventeenBananas = bananaPricePerPiece*17
+console.log("Siebzehn Bananen kosten " + seventeenBananas + "€");
+
+let oneTonApples = applePricePerKilo*1000
+console.log("Eine Tonne Äpfel kostet " + oneTonApples + "€");
+
+let oneTonBananas = bananaPricePerKilo*1000
+console.log("Eine Tonne Bananen kostet " + oneTonBananas + "€");
