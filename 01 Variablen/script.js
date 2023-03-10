afed2ad850780a0894172b0b90bc7dcde2fffcdc
David <david.sienz@outlook.com>
2023-03-10 09:39:55 +0100

    überarbeitet

--- 01 Variablen/script.js
+++ 01 Variablen/script.js
@@ -1,2 +1,44 @@
-console.log("hallo");
-// test
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+let result = a + b + c;
+console.log("a + b + c = " + result);
+
+result = (a + b) / c;
+console.log("(a + b) /c = " + result);
+
+result = c - a + b;
+console.log("c - a + b = " + result);
+
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result); 
+
+//Rechnungen mit veränderten Variablen
+a = 3;
+b = a + 3;
+c = c - 3;
+
+result = a + b + c;
+console.log("a + b + c = " + result);
+
+result = (a + b) / c;
+console.log("(a + b) /c = " + result);
+
+result = c - a + b;
+console.log("c - a + b = " + result);
+
+result = a + b + c * 3;
+console.log("a + b + c * 3 = " + result);
+
+result = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + result);
+
+console.log("😎");
