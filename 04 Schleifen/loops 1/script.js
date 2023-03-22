32ea9a6ffb5ef4fe7455e0f12c124b825d092602
David <david.sienz@outlook.com>
2023-03-22 09:43:47 +0100

    04 - Schleifen fertig gestellt

--- 04 Schleifen/loops 1/script.js
+++ 04 Schleifen/loops 1/script.js
@@ -19,20 +19,34 @@ for (let count2 = 9; count2 <= 99; count2++) {
 
 console.log("Fertig mit 2.Übung")
 
-for (let count3 = 10; count3 >= 0; count3--){
+for (let count3 = 10; count3 >= 0; count3--) {
     console.log(count3)
 }
 
 console.log("Fertig mit 3.Übung")
 
-for (let count4 = 1234; count4 >= 1207; count4--){
+for (let count4 = 1234; count4 >= 1207; count4--) {
     console.log(count4)
 }
 
 console.log("Fertig mit 4.Übung")
 
-for (let name = 0; name>=8; name++){
-    console.log(name + "David")
+for (let name = 1; name <= 8; name++) {
+    console.log("David")
 }
 
 console.log("Fertig mit 5.Übung")
+
+for (let count5 = 1; count5 <= 10; count5++){
+    console.log(count5)
+
+    if(count5 === 10){
+        console.log("David")
+        
+        for(let count6=10; count6 >= 0; count6--){
+            console.log(count6)
+        }
+    }
+}
+
+
