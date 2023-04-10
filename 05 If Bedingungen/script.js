9d451ccf23f933ab7010159b3a1db5ff4a146cde
David <david.sienz@outlook.com>
2023-04-10 09:43:17 +0200

    05 If-Bedingungen: Fertig gestellt

---
+++ 05 If Bedingungen/script.js
@@ -0,0 +1,32 @@
+// erstelle eine Variable number
+let x = 38;
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+        if (x<20){
+            console.log("Mini");
+
+        } else if (x<50){
+            console.log("Medium");
+
+        } else if (x>50){
+            console.log("Large");
+
+        }
+
+// Aufgabe 2S
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+        if (x<=7){
+            console.log("Mini");
+
+        } else if(x<90){
+            console.log("Medium")
+
+        } else if(x>90){
+            console.log("Large")
+            
+        }
