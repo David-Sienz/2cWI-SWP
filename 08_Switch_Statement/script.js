fd587505217f6807634b2f275ad3259c97885a5d
David <david.sienz@outlook.com>
2023-04-12 08:09:44 +0200

    08_Switch-Statement: fertig gestellt

---
+++ 08_Switch_Statement/script.js
@@ -0,0 +1,23 @@
+let randomNumber = Math.random() * 6 + 5;
+let x = Math.floor(randomNumber);
+
+switch (x){
+    case 10:
+        console.log("ten");
+        break;
+    case 9:
+        console.log("nine");
+        break;
+    case 8:
+        console.log("eight");
+        break;
+    case 7:
+        console.log("seven");
+        break;
+    case 6:
+        console.log("six");
+        break;
+    case 5:
+        console.log("five");
+        break;
+}
