9233e13c387677ebc4868f12e2603538ab1ff95f
David <david.sienz@outlook.com>
2023-04-12 09:43:15 +0200

    10_Funktionen: 4/5 abgeschlossen

---
+++ 10_Funktionen/script.js
@@ -0,0 +1,19 @@
+function addition(a,b){
+    return a + b;
+}
+console.log(addition(10, 5));
+
+function subtraktion(a,b){
+    return a - b;
+}
+console.log(subtraktion(10, 5));
+
+function multiplikation(a,b){
+    return a * b;
+}
+console.log(multiplikation(10, 5));
+
+function supercalculation(a,b){
+    return (a+b)/2 * a;
+}
+console.log(supercalculation(10, 5));
