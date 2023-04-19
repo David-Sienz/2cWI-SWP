b9da5ac5cb4e3d1420bf32f0fb5beecfd3f086f8
David <david.sienz@outlook.com>
2023-04-19 12:00:59 +0200

    10_Funktionen: finished

--- 10_Funktionen/script.js
+++ 10_Funktionen/script.js
@@ -17,3 +17,21 @@ function supercalculation(a,b){
     return (a+b)/2 * a;
 }
 console.log(supercalculation(10, 5));
+
+function printEasterDate(year){
+    n = year - 1900
+    a = n % 19
+    b = ((7 * a + 1) / 19)
+    m = (11 * a * 4 - b) % 29
+    q = Math.floor (n/4)
+    w = (n + q + 31 - m) % 7
+    p = Math.floor (25 - m - w)
+
+    if (p > 0){
+        console.log("Ostersonntag ist der " + p + ". April")
+    } else{
+        console.log("Ostersonntag ist der " + (p + 31) + ". März")
+    }
+}
+
+printEasterDate(1843);
