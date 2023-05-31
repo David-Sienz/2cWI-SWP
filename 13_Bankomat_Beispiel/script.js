3030264f2a0b3d72289139e6fadbc22a6f81dc29
David <david.sienz@outlook.com>
2023-05-31 09:22:26 +0200

    13_Bankomat_Beispiel: Überarbeitet/Verbessert

--- 13_Bankomat_Beispiel/script.js
+++ 13_Bankomat_Beispiel/script.js
@@ -1,17 +1,28 @@
 import { createInterface } from "readline";
 
-const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout,
+let readline = createInterface({
+
+input: process.stdin,
+
+output: process.stdout
+
 });
 
-const readLineAsync = () => {
-    return new Promise((resolve) => {
-        readline.question("", (userRes) => {
-            resolve(userRes);
-            // readline.close();
-        });
-    });
+
+
+
+let readLineAsync = () => {
+
+return new Promise((resolve) => {
+
+ readline.question("", (userRes) => {
+
+ resolve(userRes);
+
+});
+
+});
+
 };
 
 let running = true;
